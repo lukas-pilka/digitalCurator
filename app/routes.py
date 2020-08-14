2662699a0be422ae2ebea7b2c958ef9162271d1f
Lukas Pilka <pilka@blueghost.cz>
2020-08-14 11:36:49 +0200

    Adds charts with objects by periods

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-
 # IMPORTS
 
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 import config
+
 
 # CONNECTION TO ELASTIC SEARCH
 
@@ -16,6 +16,7 @@ def callElastic(query):
     rawData.encoding = 'utf-8'
     dataDict = json.loads(rawData.text)
     return (dataDict)
+
 
 # Query returning frequent object types
 
@@ -32,7 +33,7 @@ getObjectTypes = {
 }
 
 objectTypesDict = callElastic(getObjectTypes)
-minObjectsLimit = 1
+minObjectsLimit = 100
 objectTypes = []
 
 # Selecting object types with more than 10 presences
@@ -46,8 +47,8 @@ from random import randrange
 randomObjectType = randrange(len(objectTypes))
 searchedObject = objectTypes[randomObjectType]
 
-def getArtworksByObject(searchedObject):
 
+def getArtworksByObject(searchedObject):
     # Query for finding artworks containing selected object
 
     getByObject = {
@@ -82,10 +83,13 @@ def getArtworksByObject(searchedObject):
     # expanding data by imageUrl and topObjectScore
 
     expandedArtworks = []
+    earliestWork = 10000
+    latestWork = -10000
 
     for artwork in artworks:
         # generates image url
-        imageUrl = 'https://storage.googleapis.com/digital-curator.appspot.com/artworks-all/' + artwork['_id'] + '.jpg'  # Creating img url from artwork id
+        imageUrl = 'https://storage.googleapis.com/digital-curator.appspot.com/artworks-all/' + artwork[
+            '_id'] + '.jpg'  # Creating img url from artwork id
         artwork['_source']['image_url'] = imageUrl
         # rounds object score
         for object in artwork['_source']['detected_objects']:
@@ -99,11 +103,13 @@ def getArtworksByObject(searchedObject):
         expandedArtworks.append(artwork)
     return expandedArtworks
 
+
 # sorts expandedArtworks by topObjectScore
 
 def sorter(artwork):
     score = artwork['_source']['top_object_score']
     return (score)
+
 
 # get collection sum
 
@@ -128,8 +134,69 @@ getCollectionsSum = {
     }
 }
 
+
+# counting objects in periods
+
+def objectsByPeriods(searchedObject,interval):
+    allArtworksByPeriod = {
+        "size": 0,
+        "query": {
+            "bool": {
+                "must": {
+                    "exists": {
+                        "field": "detected_objects"
+                    }
+                }
+            }
+        },
+        "aggs": {
+            "periods": {
+                "histogram": {
+                    "field": "date_latest",
+                    "interval": interval
+                }
+            }
+        }
+    }
+    realatedArtworksByPeriod = {
+        "size": 0,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "match": {
+                            "detected_objects.object": searchedObject
+                        }
+                    }
+                ]
+            }
+        },
+        "aggs": {
+            "periods": {
+                "histogram": {
+                    "field": "date_latest",
+                    "interval": interval
+                }
+            }
+        }
+    }
+    countAll = callElastic(allArtworksByPeriod)['aggregations']['periods']['buckets'] # Gets count of all artworks in specific periods
+    countWithObject = callElastic(realatedArtworksByPeriod)['aggregations']['periods']['buckets'] # Gets count of artworks copntained selected object in specific periods
+
+    minArtworksLimit = 10
+
+    relatedPeriods = []
+    for periodSet in countWithObject: # Gets labels
+        if periodSet['doc_count'] > minArtworksLimit or len(relatedPeriods) > 0: # cuts periods with small number of artworks but only from beginning of timeline
+            relatedPeriods.append(periodSet['key'])
+    relatedArtworks = [periodSet['doc_count'] for periodSet in countWithObject if periodSet['key'] in relatedPeriods] # check if period is in relatedPeriods and if so, it adds cout to related artworks
+    totalArtworks = [periodSet['doc_count'] for periodSet in countAll if periodSet['key'] in relatedPeriods] # check if period is in relatedPeriods and if so, it adds count to totoal artworks
+    relatedPercents = [round(relatedArtworks[item] / totalArtworks[item],3) * 100 for item in range(len(totalArtworks))] # Counting percent of artworks copntained selected object in comparison with total artworks
+    artworksInPeriod = [relatedPeriods, relatedPercents, relatedArtworks, totalArtworks]
+    return artworksInPeriod
+
 '''
-artworksForSorting = getArtworksByObject(searchedObject)
+artworksForSorting = getArtworksByObject(searchedObject)[0]
 artworksSorted = sorted(artworksForSorting, key=sorter, reverse=True)
 for artwork in artworksSorted:
     print(artwork['_source']['title'],artwork['_source']['top_object_score'])
@@ -142,17 +209,20 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    maxGallerySize = 10
+    maxGallerySize = 50
     randomObjectType = randrange(len(objectTypes))
     searchedObject = objectTypes[randomObjectType]
     artworksForSorting = getArtworksByObject(searchedObject)
     artworksSorted = sorted(artworksForSorting, key=sorter, reverse=True)[:maxGallerySize]
     collectionsSum = callElastic(getCollectionsSum)['aggregations']['galleries_sum']['buckets']
+    artworksInPeriod = objectsByPeriods(searchedObject,50)
     return render_template('index.html',
                            artworksForWeb=artworksSorted,
                            searchedObject=searchedObject,
-                           collectionsSum=collectionsSum
+                           collectionsSum=collectionsSum,
+                           artworksInPeriod=artworksInPeriod
                            )
 @app.route('/test')
 def test():
     return render_template('test.html')
+
