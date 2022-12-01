7cac725d510554a0622765d335f960cd4fa624be
Lukas Pilka <pilka@blueghost.cz>
2022-12-01 12:03:05 +0100

    Updates images

---
+++ backend/googleAuth.py
@@ -0,0 +1,16 @@
+import os
+import config
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="../"+config.googleCredentialsKey
+
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+implicit()
